ab5472ede3d68b3070aee58ec72c5252c3dbe1b3
Michael Gatewood <michael@coincloudnv.com>
2024-10-15 15:48:28 -0700

    Add hero page

---
+++ components/Hero.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import { BorderMagicButton } from './ui/BorderMagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
+
+export const Hero = () => {
+  return (
+		<div className="pb-20 pt-36">
+			<div>
+				<Spotlight className="-top-40 -left-10 md:-top-20 md:-left-32 h-screen fill:white"/>
+				<Spotlight className="top-10 left-full h-[80vh] w-[50vw] fill:purple"/>
+				<Spotlight className="top-28 left-80 h-[80vh] w-[50vw] fill:blue"/>
+			</div>
+			<div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center">
+				{/* Radial gradient for the container to give a faded look */}
+				<div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+				<p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8"/>
+			</div>
+		  	<div className="flex justify-center relative my-20 z-10">
+				<div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col justify-center items-center">
+					<h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Michael N. Gatewood</h2>
+					<TextGenerateEffect
+						className="text-[40px] md:text-5xl lg:text-6xl text-center"
+						words="Cross-Functional Creator"
+					/>
+					<p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">I want to build things for you.</p>
+					<a href="#" title="See My Projects">
+						<BorderMagicButton
+							text="See My Projects"
+							icon={<FaLocationArrow />}
+							position="right"
+						/>
+					</a>
+				</div>
+			</div>
+		</div>
+	)
+}
