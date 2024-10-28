bd0e3105775199683a2b6b3f00e325a5e61c53b1
Michael Gatewood <michael@coincloudnv.com>
2024-10-28 13:03:20 -0700

    Add tooltip to project icons

--- components/ProjectIcon.tsx
+++ components/ProjectIcon.tsx
@@ -14,7 +14,13 @@ export const ProjectIcon = ({title}: {title: string}) => {
 
 	return (
 		<IconContext.Provider value={{ color: '#38BDF8', size: "1.5rem" }} >
-			{iconElement}
+			<div className="group">
+				{iconElement}
+				<div className='absolute -top-16 px-4 py-2 bg-black-100 rounded-md text-purple border-gray-500 border left-0 text-lg opacity-0 group-hover:opacity-100 transition-all duration-200 transform scale-0 group-hover:scale-100'>
+					<span>{title}</span>
+					<span className="h-0 w-0 absolute -bottom-2 left-2 lg:left-3 border-x-8 border-x-transparent border-t-8 border-t-gray-500"></span>
+				</div>
+			</div>
 		</IconContext.Provider>
 	)
 }
