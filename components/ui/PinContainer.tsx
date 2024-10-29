4b615272fb2857572188d1d1c16f9ddc5869f3ec
Michael Gatewood <michael@coincloudnv.com>
2024-10-29 10:08:42 -0700

    Adjust PinContainer to be responsive to touch

--- components/ui/PinContainer.tsx
+++ components/ui/PinContainer.tsx
@@ -1,8 +1,7 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, MouseEvent } from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
-import Link from "next/link";
 
 export const PinContainer = ({
 	children,
@@ -17,26 +16,64 @@ export const PinContainer = ({
 	className?: string;
 	containerClassName?: string;
 }) => {
+
 	const [transform, setTransform] = useState(
 		"translate(-50%,-50%) rotateX(0deg)"
 	);
+	const [isHovering, setIsHovering] = useState(false);
+
+	const toggleTransform = () => {
+		if (isHovering) {
+			setTransform("translate(-50%,-50%) rotateX(0deg) scale(1)");
+		} else {
+			setTransform("translate(-50%,-50%) rotateX(40deg) scale(0.8)");
+		}
+		setIsHovering(!isHovering);
+	};
 
 	const onMouseEnter = () => {
 		setTransform("translate(-50%,-50%) rotateX(40deg) scale(0.8)");
+		setIsHovering(true);
 	};
 	const onMouseLeave = () => {
 		setTransform("translate(-50%,-50%) rotateX(0deg) scale(1)");
+		setIsHovering(false);
+	};
+
+	const isTouchEvent = (event: MouseEvent | PointerEvent): event is PointerEvent => {
+		if ('nativeEvent' in event && 'pointerType' in event.nativeEvent && event.nativeEvent.pointerType === "touch") {
+			return true;
+		}
+		return false;
+	};
+
+	const handleClick = (event: MouseEvent | PointerEvent) => {
+		const touch = isTouchEvent(event);
+		// If the event is a touch event, toggle the transform only
+		if (touch) {
+			toggleTransform();
+		} else {
+			// If the event is a mouse or pen event and the element is already in a hover state, then redirect to the link
+			if (isHovering && href) {
+				// Redirect to the link when the element is clicked
+				window.open(href, '_blank');
+			// If the event is a mouse or pen event and the element is not in a hover state, then toggle the transform only
+			} else {
+				toggleTransform();
+			}
+		}
 	};
 
 	return (
-		<Link
+		<div
 			className={cn(
 				"relative group/pin z-50 cursor-pointer",
 				containerClassName
 			)}
 			onMouseEnter={onMouseEnter}
 			onMouseLeave={onMouseLeave}
-			href={href || "/"}
+			onClick={handleClick}
+			// href={href || "/"}
 		>
 			<div
 				style={{
@@ -49,40 +86,45 @@ export const PinContainer = ({
 					style={{
 						transform: transform,
 					}}
-					className="absolute left-1/2 p-4 top-1/2 flex justify-start items-start rounded-2xl shadow-[0_8px_16px_rgb(0_0_0/0.4)] bg-black border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
+					className="absolute left-1/2 p-4 top-1/2 flex justify-start items-start rounded-2xl shadow-[2px_2px_6px] shadow-purple bg-black border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
 				>
 					<div className={cn(" relative z-50 ", className)}>{children}</div>
 				</div>
 			</div>
-			<PinPerspective title={title} href={href} />
-		</Link>
+			<PinPerspective title={title} href={href} isHovering={isHovering}/>
+		</div>
 	);
 };
 
 export const PinPerspective = ({
-	// href removed since it is used in the parent component
 	title,
-	// href,
+	href,
+	isHovering
 }: {
 	title?: string;
-	// href?: string;
+	href?: string;
+	isHovering: boolean
 }) => {
 	return (
-		<motion.div className="pointer-events-none  w-96 h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
-			<div className=" w-full h-full -mt-7 flex-none  inset-0">
-				<div className="absolute top-0 inset-x-0  flex justify-center">
+		<motion.div className="w-96 h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
+			<div className=" w-full h-full -mt-7 flex-none  inset-0" style={{ visibility: isHovering ? "visible" : "hidden" }}>
+				{/* Make this div hidden when hovered = false */}
+				<div className="absolute top-0 inset-x-0 flex justify-center">
 					{/* Changed <a> tag to <div> in order to prevent hydration errors due to nested <a> tags */}
-					<div
-						// href={href}
-						// target={"_blank"}
+					<a
+						href={href}
+						target={"_blank"}
 						className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
 					>
-						<span className="relative z-20 text-white text-xs md:text-sm font-bold inline-block py-0.5">
-							{title}
-						</span>
+						<div>
+							<span className="relative z-20 text-white text-xs md:text-sm font-bold inline-block py-0.5">
+								{title}
+							</span>
 
-						<span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></span>
-					</div>
+							<span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></span>
+						</div>
+						<div className="absolute -bottom-5 -left-2 w-full text-xs text-center font-extralight uppercase">Go to live site</div>
+					</a>
 				</div>
 
 				<div
